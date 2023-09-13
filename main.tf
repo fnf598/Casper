c3d0a76dc12417ed13123a82faf3d8abeb94ba69
fnf598 <82993942+fnf598@users.noreply.github.com>
2023-09-13 12:42:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "aws_launch_template" "foo" {
 
 
   metadata_options {
-    http_endpoint               = "enabled"git branch -M main
+    http_endpoint               = "enabled"
     http_tokens                 = "required"
     http_put_response_hop_limit = 1
     instance_metadata_tags      = "enabled"
