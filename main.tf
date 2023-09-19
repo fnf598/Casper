65962c01fe03ce801d081319fa7b70b817739847
fnf598 <82993942+fnf598@users.noreply.github.com>
2023-09-19 12:37:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,6 +4,7 @@ resource "aws_launch_template" "foo" {
 
   metadata_options {
     http_endpoint               = "enabled"
+    https_tokens                = "required"
     http_put_response_hop_limit = 1
     instance_metadata_tags      = "enabled"
   }
