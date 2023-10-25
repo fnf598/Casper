a065f04a3d54b4f44eaf0b4613490dff57693b75
fnf598 <82993942+fnf598@users.noreply.github.com>
2023-10-25 13:11:45 +0530

    Compliant 3

--- main.tf
+++ main.tf
@@ -37,29 +37,6 @@ provider "aws" {
 
 ##Compliant 2
 
-resource "aws_iam_policy" "policy" {
-  name        = "test_policy"
-  path        = "/"
-  description = "My test policy"
-
-  # Terraform's "jsonencode" function converts a
-  # Terraform expression result to valid JSON syntax.
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = [
-          "ec2:Describe*"
-        ]
-        Effect   = "Allow"
-        Resource = "*"
-      },
-    ]
-  })
-}
-
-# ##Compliant 3
-
 # resource "aws_iam_policy" "policy" {
 #   name        = "test_policy"
 #   path        = "/"
@@ -77,16 +54,39 @@ resource "aws_iam_policy" "policy" {
 #         Effect   = "Allow"
 #         Resource = "*"
 #       },
-#       {
-#         Action = [
-#           "iam:*"
-#         ]
-#         Effect   = "Deny"
-#         Resource = "*"
-#       }
 #     ]
 #   })
 # }
+
+# ##Compliant 3
+
+resource "aws_iam_policy" "policy" {
+  name        = "test_policy"
+  path        = "/"
+  description = "My test policy"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ec2:Describe*"
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+      {
+        Action = [
+          "iam:*"
+        ]
+        Effect   = "Deny"
+        Resource = "*"
+      }
+    ]
+  })
+}
 
 # ##Non Compliant 1
 
